38d2d615f3e074ab6c7c672099f0dc0e0dc8e66f
yalh76 <yalh@yahoo.com>
2022-03-22 20:43:20 +0100

    Updating php dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 YNH_PHP_VERSION="8.0"
 
-pkg_dependencies="curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php-php-gettext php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-curl"
+pkg_dependencies="php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-smbclient php${YNH_PHP_VERSION}-ssh2  php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip"
 
 #=================================================
 # PERSONAL HELPERS
